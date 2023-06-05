fa6c09d4456dbee1a185584716916697d34c0383
YunL97 <leeys9932@naver.com>
2023-06-05 21:10:50 +0900

    next강의 89강까지

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,46 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import path from 'path';
+import fs from 'fs/promises';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+import Link from 'next/link';
+
+function HomePage(props) {
+  const { products } = props;
 
   return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          <Link href={`/products/${product.id}`}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
   );
+}
+
+export async function getStaticProps(context) {
+  console.log('(Re-)Generating...');
+  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: '/no-data',
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
 }
 
 export default HomePage;
